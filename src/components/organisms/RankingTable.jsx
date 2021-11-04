b4286f30baa781a7d248626637d20f3234ae2a24
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2021-11-04 16:44:24 +0900

    invite後のレンダリングに成功

--- src/components/organisms/RankingTable.jsx
+++ src/components/organisms/RankingTable.jsx
@@ -74,7 +74,7 @@ export const RankingTable = () => {
               });
             })
             .catch((error) => {
-              console.log(error);
+              console.log(error.response);
             });
         }));
         tmpArray = tmpArray.sort((a, b) => {
@@ -86,7 +86,9 @@ export const RankingTable = () => {
       }
     };
     GetGithubData();
-  }, [roomInfo.members]);
+  }, [roomInfo]);
+
+  console.log(githubData)
 
   return (
     <div>
