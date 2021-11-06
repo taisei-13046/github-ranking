7e21a21cd5dd4aae2a19fedb519bb2eb3785baa5
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2021-11-06 16:08:03 +0900

    おそらくうまくいっている

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { createContext, useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router/Router";
-import { auth } from "./firebase"
-import { Home } from "./components/pages/Home"
+import { auth } from "./firebase";
+import { Home } from "./components/pages/Home";
 
-export const UserEmailContext = createContext()
-export const UserGithubContext = createContext()
-export const RoomInfoContext = createContext()
+export const UserEmailContext = createContext();
+export const UserGithubContext = createContext();
+export const RoomInfoContext = createContext();
 
 const App = () => {
-  const [userEmail, setUserEmail] = useState("")
-  const [githubId, setGithubId] = useState("")
-  const [roomInfo, setRoomInfo] = useState({})
-  const [flag, setFlag] = useState(false)
+  const [userEmail, setUserEmail] = useState("");
+  const [githubId, setGithubId] = useState("");
+  const [roomInfo, setRoomInfo] = useState({});
+  const [flag, setFlag] = useState(false);
   useEffect(() => {
     const unSub = auth.onAuthStateChanged((authUser) => {
       if (authUser) {
-        console.log(authUser)
-        setUserEmail(authUser.email)
+        console.log(authUser);
+        setUserEmail(authUser.email);
       }
     });
     return () => {
@@ -25,25 +25,27 @@ const App = () => {
     };
   }, [setUserEmail]);
 
-  console.log(userEmail)
+  var oneWeekAgoDate = new Date();
+  oneWeekAgoDate.setDate(oneWeekAgoDate.getDate() - 7);
+  console.log(oneWeekAgoDate);
 
   return (
     <RoomInfoContext.Provider value={{ roomInfo, setRoomInfo }}>
       <UserEmailContext.Provider value={{ userEmail, setUserEmail }}>
         <UserGithubContext.Provider value={{ githubId, setGithubId }}>
-          {userEmail && flag ?
+          {userEmail && flag ? (
             <>
               <BrowserRouter>
                 <Router />
               </BrowserRouter>
             </>
-            : (
-              <Home flag={flag} setFlag={setFlag} />
-            )}
+          ) : (
+            <Home flag={flag} setFlag={setFlag} />
+          )}
         </UserGithubContext.Provider>
       </UserEmailContext.Provider>
     </RoomInfoContext.Provider>
   );
-}
+};
 
 export default App;
